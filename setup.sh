455ce33d1cbb18faf83de52e4f16bfa53a08ff89
quangnghi99 <quangnghi2402@gmail.com>
2022-06-14 15:52:43 +0700

    fisrt commit

---
+++ setup.sh
@@ -0,0 +1,20 @@
+# docker 
+sudo yum update
+sudo yum install docker
+sudo usermod -a -G docker $USER 
+sudo service docker start
+
+#kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+#minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+minikube start
+
+#helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh

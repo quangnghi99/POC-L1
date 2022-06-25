58b866af179b4b2a8baf454ef99020aaadfd618e
quangnghi99 <84608260+quangnghi99@users.noreply.github.com>
2022-06-25 19:48:09 +0700

    Update setup-jenkin.sh

--- setup-jenkin.sh
+++ setup-jenkin.sh
@@ -3,7 +3,7 @@ sudo yum install java-1.8.0 -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade
+sudo yum upgrade -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
